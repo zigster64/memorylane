8879b8b8a3b68e4216423a4e6dfd64c0ddfa44ea
zigster64 <steve@raffmail.net>
2021-03-16 21:53:51 +1000

    More styling
    - On mobile, take full width and merge the pics into col
    - Add release details

--- lib/mems_web/views/layout_view.ex
+++ lib/mems_web/views/layout_view.ex
@@ -1,14 +1,23 @@
 defmodule MemsWeb.LayoutView do
   use MemsWeb, :view
 
+  def name("index.html") do
+    "Home"
+  end
+
+  def name(template) do
+    [n|_] = String.split(template, ".")
+    String.capitalize(n)
+  end
+
   def nav_link(what, title, template) do
     if String.starts_with?(template, what) do
     raw("""
-    <a class="ml-5 px-2 py-1 text-blue-400 hover:text-blue-800 lg:text-yellow-300 lg:hover:text-yellow-200" href="/#{what}">#{title}</a>
+    <a class="ml-5 px-2 py-1 text-yellow-300 hover:text-yellow-200" href="/#{what}">#{title}</a>
     """)
     else
     raw("""
-    <a class="ml-5 px-2 py-1 text-gray-400 hover:text-gray-800 lg:text-gray-200 lg:hover:text-gray-400" href="/#{what}">#{title}</a>
+    <a class="ml-5 px-2 py-1 text-gray-200 hover:text-gray-400" href="/#{what}">#{title}</a>
     """)
     end
   end
